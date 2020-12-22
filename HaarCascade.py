b06b78062403931905281a20797f26da8a9ba5f2
Your Name <hayri.durmaz@tedu.edu.tr>
2020-12-22 22:38:11 +0300

    video capture added

--- HaarCascade.py
+++ HaarCascade.py
@@ -11,7 +11,12 @@ if __name__ == '__main__':
         img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
-
+    # According to Google[21], the face alignment increases the accuracy of its
+    # face recognition model FaceNet from 98.87 to 99.63.
+    camera = cv2.VideoCapture(0)
+    camera.set(3, 640)
+    camera.set(4, 480)
+    recognizer = cv2.face.LBPHFaceRecognizer_create(radius=1, neighbors=8, grid_x=8, grid_y=8)
     cv2.imshow('img', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
