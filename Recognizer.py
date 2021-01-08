2ad28c99b0f88d6f1cd0d0a95749b3a8099d6851
Your Name <hayri.durmaz@tedu.edu.tr>
2021-01-09 01:09:21 +0300

    user methods formatted (compile error persists)

--- Recognizer.py
+++ Recognizer.py
@@ -5,18 +5,23 @@ import time
 import numpy as np
 import argparse as arg
 import matplotlib.pyplot as plt
+import logging
 
 import config
-from Utilities import create_if_not_exist
+from Utilities import create_if_not_exist, create_dataset_for_user
 
 numberOfsamples = config.recognizer_options['numberOfsamples']
 dataset_name = config.recognizer_options['dataset_name']
 file_name = config.recognizer_options['file_name']
 
+dataset_path = config.recognizer_options['user_dataset']
+
 
 class Recognizer:
-    def __init__(self, face_cascade, recognizer):
-        self._Face_Cascade = cv2.CascadeClassifier(face_cascade)
+    def __init__(self, recognizer):
+        self._Face_Cascade = cv2.CascadeClassifier(config.cascade_files['face_cascade_path'])
+        self._Right_Eye_Cascade = cv2.CascadeClassifier(config.cascade_files['right_eye_cascade_path'])
+        self._Left_Eye_Cascade = cv2.CascadeClassifier(config.cascade_files['left_eye_cascade_path'])
         self.recognizer = recognizer
         create_if_not_exist("dataset/")
 
@@ -29,8 +34,8 @@ class Recognizer:
     #     Info.close()
     #     return ID
 
-    def getImagesAndLabels(self, path):
-        imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    def getImagesAndLabels(self):
+        imagePaths = [os.path.join(dataset_path, f) for f in os.listdir(dataset_path)]
         faceSamples = []
         ids = []
         for imagePath in imagePaths:
@@ -41,24 +46,21 @@ class Recognizer:
             ids.append(id)
         return faceSamples, ids
 
-    def train(self, path, file_name):
-        print("\n[INFO] Face training has been started, please wait a moment...")
+    def train(self):
+        logging.info("Training...")
         # slight delay
         time.sleep(1)
-        faces, ids = self.getImagesAndLabels(path)
+        faces, ids = self.getImagesAndLabels()
         self.recognizer.update(faces, np.array(ids))
         # Saving the model
         self.recognizer.write(file_name)
-        print("\n[INFO] {0} persons trained successfully.".format(len(np.unique(ids))))
-        print("\n[INFO] Quitting the program")
-    def create_dataset_for_user(self):
-        
+        logging.info("{0} persons trained successfully.".format(len(np.unique(ids))))
 
-    def addNewFace(self, input, isVideo):
+    def addNewFace(self, input, isVideo, user):
         if isVideo:
             video = cv2.VideoCapture(input)
             # create a dataset for further model training
-            self.create_dataset_for_user(video)
+            create_dataset_for_user(video, user, numberOfsamples)
             # Training the model
             self.train()
         else:
