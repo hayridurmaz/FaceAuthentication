e56d6969b2efbee8c769bb495ed2183fcfdabd95
Your Name <hayri.durmaz@tedu.edu.tr>
2021-01-09 17:45:17 +0300

    serid changed as int

--- User.py
+++ User.py
@@ -1,16 +1,21 @@
 import uuid
 
-import config
+from numpy import size
 
-splitChar = ","
+import config
 
 
 class User:
+    file = open(config.user_file, "r")
+    userStrList = file.readlines()
+    ID_seq = size(userStrList)
+
     def __init__(self, id, username, name):
         self.username = username
         self.name = name
         if id is None:
-            self.id = uuid.uuid1()
+            self.id = User.ID_seq
+            User.ID_seq = User.ID_seq + 1
         else:
             self.id = id
 
@@ -34,7 +39,18 @@ def getUserByUsername(username):
     return None
 
 
+def getUserById(u_id):
+    userList = getAllUsers()
+    for user in userList:
+        if user.id == str(u_id):
+            return user
+    return None
+
+
 def addUser(User):
     file = open(config.user_file, "a")
     user_str = '{}{}{}{}{}\n'.format(User.id, splitChar, User.username, splitChar, User.name)
     file.write(user_str)
+
+
+splitChar = ","
