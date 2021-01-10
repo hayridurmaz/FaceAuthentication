7048172947b6204fe134123ad88874533911f62c
Your Name <hayri.durmaz@tedu.edu.tr>
2021-01-10 06:01:03 +0300

    main updated

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def authenticate(model):
 def testCase(model):
     # Comment unneeded lines!
     # addUsers()
-    train(model)
+    # train(model)
     authenticate(model)
 
 
@@ -61,36 +61,3 @@ if __name__ == '__main__':
     model = Recognizer(recognizer)
 
     testCase(model)
-
-    # hayri = User(None, "hayri", "hdurmaz")
-    # addUser(User(None, "hayri", "hayri"))
-    # addUser(User(None, "akadir", "akadir"))
-    users = getAllUsers()
-
-    # model.addNewFace(None, False, users[0])
-    # model.addNewFace(None, False, user=users[1])
-
-    result = model.queryFace(None, users[0])
-
-    if result:
-        logging.info("YES...")
-    else:
-        logging.error("NO...")
-    # image_path = "images/hayri.png"
-
-    # logging.info(image_path)
-    # img = cv2.imread(image_path)
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # faces = model.Face_Cascade.detectMultiScale(gray, 1.3, 5)
-    # for (x, y, w, h) in faces:
-    #     img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    #     roi_gray = gray[y:y + h, x:x + w]
-    #     roi_color = img[y:y + h, x:x + w]
-    # # According to Google[21], the face alignment increases the accuracy of its
-    # # face recognition model FaceNet from 98.87 to 99.63.
-    # camera = cv2.VideoCapture(0)
-    # camera.set(3, 640)
-    # camera.set(4, 480)
-    # cv2.imshow('img', img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
