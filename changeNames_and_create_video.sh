d741ab1b8841bf3711d4e355709e2d2d50cf45ef
Your Name <hayri.durmaz@tedu.edu.tr>
2021-01-10 15:34:31 +0300

    Create changeNames_and_create_video.sh

---
+++ changeNames_and_create_video.sh
@@ -0,0 +1,10 @@
+images=$(for i in `ls *.jpg`; do LEN=`expr length $i`; echo  $i; done | sort -n)
+j=1
+for i in $images; do
+  new=$(printf "%04d.jpg" "$j") #04 pad to length of 4
+  mv -i -- "$i" "$new"
+  let j=j+1
+   #we can add a condition on j to rename just the first 999 images.
+done
+
+ffmpeg -framerate 25 -i %04d.jpg   -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p $1.mp4
