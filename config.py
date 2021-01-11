76c87d6620260c934732c02a49826e8a11e1a129
Your Name <hayri.durmaz@tedu.edu.tr>
2021-01-12 02:52:47 +0300

    eye blink detection haarcascade added

--- config.py
+++ config.py
@@ -12,13 +12,14 @@ recognizer_options = dict(
     user_dataset='dataset/users/',
     file_name='train.yaml',
     camera_id=0,
-    timeout=10,
-    number_of_recognizing_threshold=5,
-    confident_threshold=130
+    timeout=100,
+    number_of_recognizing_threshold=2,
+    confident_threshold=200
 )
 user_file = 'dataset/users.csv'
 cascade_files = dict(
     face_cascade_path=''.join([cv2.data.haarcascades, 'haarcascade_frontalface_default.xml']),
     right_eye_cascade_path=''.join([cv2.data.haarcascades, 'haarcascade_righteye_2splits.xml']),
-    left_eye_cascade_path=''.join([cv2.data.haarcascades, 'haarcascade_lefteye_2splits.xml'])
+    left_eye_cascade_path=''.join([cv2.data.haarcascades, 'haarcascade_lefteye_2splits.xml']),
+    both_eye_cascade_path=''.join([cv2.data.haarcascades, 'haarcascade_eye_tree_eyeglasses.xml'])
 )
